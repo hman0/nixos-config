1bc192746c38364c7ae943719f3e49ce19fe8888
hman0 <hman.c@proton.me>
2025-11-27 09:48:06 +0000

    feat: update zsh plugin management + add plugins

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -2,37 +2,68 @@
 {
   programs.zsh = {
     enable = true;
+     plugins = [
+      {
+        name = "fast-syntax-highlighting";
+        src = pkgs.zsh-fast-syntax-highlighting;
+        file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
+      }
+      {
+        name = "zsh-history-substring-search";
+        src = pkgs.zsh-history-substring-search;
+        file = "share/zsh-history-substring-search/zsh-history-substring-search.zsh";
+      }
+      {
+        name = "zsh-you-should-use";
+        src = pkgs.zsh-you-should-use;
+        file = "share/zsh/plugins/you-should-use/you-should-use.plugin.zsh";
+      }
+      {
+        name = "zsh-z";
+        file = "zsh-z.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "agkozak";
+          repo = "zsh-z";
+          rev = "cf9225feebfae55e557e103e95ce20eca5eff270";
+          sha256 = "sha256-C79eSOaWNHSJiUGmHzu9d0zO0NdW+dktK21a2niPZm0=";
+        };
+      }
+    ];
+    enableCompletion = true;
     autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
+
+    completionInit = ''
+      autoload -Uz compinit
+      setopt EXTENDEDGLOB
+      for dump in ~/.zcompdump(#qN.m1); do
+        compinit
+        if [[ -s "$dump" && (! -s "$dump.zwc" || "$dump" -nt "$dump.zwc") ]]; then
+          zcompile "$dump"
+        fi
+      done
+      compinit -C
+      unsetopt EXTENDEDGLOB
+    '';
     shellAliases = {
       ssh = "TERM=xterm-256color ssh";
       ff = "fastfetch";
+      cd = "z";
+    };
+    history = {
+      size = 1000;
+      save = 1000;
+      ignoreAllDups = true;
     };
     initContent = ''
-    HISTSIZE=10000
-    SAVEHIST=10000
-    HISTCONTROL=ignoredups
-    HISTFILE=~/.zsh_history
+      bindkey "$terminfo[kcuu1]" history-substring-search-up
+      bindkey "$terminfo[kcud1]" history-substring-search-down
 
-    autoload -U compinit
-    compinit
-
-    autoload -U up-line-or-beginning-search down-line-or-beginning-search
-    zle -N up-line-or-beginning-search
-    zle -N down-line-or-beginning-search
-
-    bindkey "^[[A" up-line-or-beginning-search
-    bindkey "^[[B" down-line-or-beginning-search
-
-    setopt AUTO_CD  
-    setopt NOCLOBBER
-    setopt APPEND_HISTORY
-    setopt INC_APPEND_HISTORY
-
-    export PATH="$HOME/Scripts:$PATH"
-
-    PROMPT="%F{blue}%n@%m%f %F{blue}%~ %f"
+      setopt AUTO_CD  
+      setopt NOCLOBBER
+      
+      export PATH="$HOME/Scripts:$PATH"
+      export HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND="none"
+      PROMPT="%F{blue}%n@%m%f %F{blue}%~ %f"
     '';
   };
-
 }
